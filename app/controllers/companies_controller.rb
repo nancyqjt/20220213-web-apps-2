ad496163a739d26103f0123a8446bec613ff96a9
Nancy Qian <nancyqian2023@u.northwestern.edu>
2023-02-14 02:02:56 +0000

    post controller

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -12,4 +12,17 @@ class CompaniesController < ApplicationController
     # render html file views/companies/show.html.erb
   end
 
+  def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.new
+    @company["name"] = params["company"]["name"]
+    @company["city"] = params["company"]["city"]
+    @company["state"] = params["company"]["state"]
+    @company.save
+    redirect_to "/companies"
+  end
+
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,21 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def show
+    @post = Post.find_by({ "id" => params["id"] })
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new
+    @post["author"] = params["post"]["author"]
+    @post["body"] = params["post"]["body"]
+    @post["image"] = params["post"]["image"]
+    @post.save
+    redirect_to "/posts"
+  end
+
 end
