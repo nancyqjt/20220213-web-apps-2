6024e37ccb33f47db51fbb1b7c709e3253bf9d8c
Nancy Qian <nancyqian2023@u.northwestern.edu>
2023-02-14 01:34:04 +0000

    show html

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -2,12 +2,14 @@ class CompaniesController < ApplicationController
 
   # TODO:
   def index
+    #get my data in
     @companies = Company.all 
   
   end
 
   def show
     @company = Company.find_by({ "id" => params["id"] })
+    # render html file views/companies/show.html.erb
   end
 
 end
