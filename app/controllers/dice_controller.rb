33ff205b9aa486d1a6249cbc416a4bd1d03f845d
Nancy Qian <nancyqian2023@u.northwestern.edu>
2023-02-14 01:17:18 +0000

    Create Username

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -4,6 +4,8 @@ class DiceController < ApplicationController
         @die1 = rand(1..6)
         @die2 = rand(1..6)
         total = @die1 + @die2
+        @username = params["username"]
+        # render /views/dice/index.html.erb
     end
 
 end
